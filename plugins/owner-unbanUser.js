a00c6bcfec774d5000899804f44f7972f27b01a5
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-11-15 21:31:06 +0500

    𝙳𝚂-𝙼𝙳

--- plugins/owner-unbanUser.js
+++ plugins/owner-unbanUser.js
@@ -1,17 +1,30 @@
 let handler = async (m, { conn, text, command, usedPrefix }) => {
-  if (!text) throw `Tag someone to unban.\nExample:\n*${usedPrefix + command} @tag*`;
-
+  
   let who;
-  if (m.isGroup) who = m.mentionedJid[0];
-  else who = m.chat;
+  if (m.quoted?.sender) {
+    who = m.quoted.sender;
+  } else if (m.isGroup) {
+    who = m.mentionedJid[0];
+  } else {
+    who = m.chat;
+  }
 
-  if (!who) throw `Tag someone to unban.\nExample:\n*${usedPrefix + command} @tag*`;
+  
+  if (!who) {
+    throw `Tag someone or quote their message to unban.\nExample:\n*${usedPrefix + command} @tag*`;
+  }
 
   let users = global.db.data.users;
-  if (!users[who]) throw `The user is not found in the database.`;
+
+  
+  if (!users[who]) {
+    throw `The user is not found in the database.`;
+  }
+
 
   users[who].banned = false;
 
+  
   conn.reply(
     m.chat,
     `The user has been successfully unbanned. 🎉\nThey can now use PRINCE MD.`,
