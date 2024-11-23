243f0f06af0e1fd063aae0c61b41240a5ffa8783
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-11-23 06:35:42 +0500

    𝙳𝚂-𝙼𝙳

--- config.js
+++ config.js
@@ -108,7 +108,7 @@ global.imagen11 = fs.readFileSync("./Assets/menus/img12.jpg")
 global.imag1 = fs.readFileSync("./Assets/prn.png")
 global.imag2 = fs.readFileSync("./Assets/prn1.png")
 
-global.pimg = [iamg1, imag2]
+global.pimg = [imag1, imag2]
 
 
 
