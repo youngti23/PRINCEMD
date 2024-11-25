cc5a8fa3af31b4cecb18ed885ec73a98c6110f81
GDS <112704679+DASTAGHIR@users.noreply.github.com>
2024-11-25 11:13:40 +0500

    Update config.js

--- config.js
+++ config.js
@@ -40,9 +40,9 @@ global.confirmCode = ""
 
 
 //💌global.pairingNumber = "" //put your bot number here💌
-global.mods = [923092668108'] 
-global.prems = [923092668108']
-global.allowed = [923092668108']
+global.mods = ['923092668108'] 
+global.prems = ['923092668108']
+global.allowed = ['923092668108']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
