b73032d6fddc912f94c817ed97c9f9fb8c3e14c3
GDS <112704679+DASTAGHIR@users.noreply.github.com>
2024-12-08 13:49:35 +0500

    Update

--- plugins/owner-AllowPm.js
+++ plugins/owner-AllowPm.js
@@ -1,20 +1,32 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    let who
-        if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
-        else who = m.chat
-        let user = global.db.data.users[who]
-        if (!who) throw `✳️ Tag or mention someone\n\n📌 Example : ${usedPrefix + command} @user`
-    if (global.allowed.includes(who.split`@`[0])) throw 'The user Mentioned is already allowed to use the bot in DM '
-    global.allowed.push(`${who.split`@`[0]}`)
+    let who;
     
-    conn.reply(m.chat, ` @${who.split`@`[0]} got the ultimate pass  to use the bot in DM`, m, { mentions: [who] })
-    
+    // Check if it's a group message and if there's a mention or quoted user
+    if (m.isGroup) {
+        who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
+    } else {
+        who = m.chat;
     }
-    handler.help = ['allow <@tag>']
-    handler.tags = ['owner']
-    handler.command = ['allow', 'makeallow', 'al'] 
+
+    if (!who) throw `✳️ Tag or mention someone\n\n📌 Example: ${usedPrefix + command} @user`;
     
-    handler.group = true
-    handler.rowner = true
+    let user = global.db.data.users[who];
     
-    export default handler
+    // Check if the user is already allowed
+    if (global.allowed.includes(who.split`@`[0])) throw 'The user Mentioned is already allowed to use the bot in DM';
+
+    // Add the user to the allowed list
+    global.allowed.push(`${who.split`@`[0]}`);
+    
+    // Send the confirmation reply
+    conn.reply(m.chat, `@${who.split`@`[0]} got the ultimate pass to use the bot in DM`, m, { mentions: [who] });
+};
+
+handler.help = ['allow <@tag>'];
+handler.tags = ['owner'];
+handler.command = ['allow', 'makeallow', 'alw'];
+
+handler.group = false;
+handler.rowner = true;
+
+export default handler;
