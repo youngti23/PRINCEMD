06ac9b35e2b8248882917c28049c6223793227fb
GDS <112704679+DASTAGHIR@users.noreply.github.com>
2024-12-13 18:23:39 +0500

    Update

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -3,7 +3,7 @@
 let handler = async (m, { conn, isOwner, isAdmin, isROwner }) => {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('🎯Tʜᴇ ʙᴏᴛ ʜᴀs ʙᴇᴇɴ ᴅᴇᴀᴄᴛɪᴠᴀᴛᴇᴅ ғᴏʀ ᴛʜɪs ᴄʜᴀᴛ𒁂')
+    m.reply('⚠️ Bᴏᴛ ᴅᴇᴀᴄᴛɪᴠᴀᴛᴇᴅ 🚫 \n💀 Nᴏ ᴄᴏᴍᴍᴀɴᴅs ᴡɪʟʟ ᴡᴏʀᴋ! 𒆙')
 
     // Save the updated database
     if (global.db && typeof global.db.save === 'function') {
